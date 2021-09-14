f845e5ffda7ab0c48abd0407ec2bc1c067c7453e
Rradley <robbie.rradley@gmail.com>
2021-09-14 21:14:07 +0300

    Update ba5b.py
    Fixed

--- Bioinformatics_Algorithms/Chapter_5/ba5b.py
+++ Bioinformatics_Algorithms/Chapter_5/ba5b.py
@@ -32,12 +32,12 @@ def ManhattanTourist(n,m,down,right):
         s.append(l)
     
     for i in range(1,n + 1):
-        s[i][0] = s[i-1][0] + int(down[i][0])
+        s[i][0] = s[i-1][0] + int(down[i-1][0])
     for j in range (1,m + 1):
-        s[i][0] = s[i-1][0] + int(right[i][0])
+        s[0][j] = s[0][j-1] + int(right[0][j-1])
     for i in range (1,n + 1):
         for j in range (1,m + 1):
-            s[i][j] = max(s[i-1][j] + int(down[i][j]), s[i][j-1] + int(right[i][j]))
+            s[i][j] = max(s[i-1][j] + int(down[i-1][j]), s[i][j-1] + int(right[i][j-1]))
     return s[n][m]
 
 
